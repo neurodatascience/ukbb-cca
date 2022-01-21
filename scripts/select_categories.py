594ba96a48ca2504b2ee93badc98761766098a9d
Michelle Wang <tomichellewang@gmail.com>
2022-01-21 15:30:34 -0500

    filter UDIs by instances/value type

--- scripts/select_categories.py
+++ scripts/select_categories.py
@@ -6,27 +6,35 @@ from paths import DPATHS, FPATHS
 
 if __name__ == '__main__':
 
-    chunksize = 10000
+    value_types = 'numeric' # integer, categorical (single/multiple), continuous
 
     dpath_schema = DPATHS['schema']
     fpath_udis = FPATHS['udis_tabular_raw']
     fpath_data = FPATHS['data_tabular_mri_subjects']
 
+    chunksize = 10000
+
     configs = {
         'behavioural': {
-            'categories': [100026, 116],
-            'fpath_out': FPATHS['data_behavioural'],
+            'categories': [100026], # [100026, 116]
             'filter_value': None,
+            'instances': [2],
+            'keep_instance': 'all',
+            'fpath_out': FPATHS['data_behavioural'],
         },
         'brain': {
             'categories': [134],
-            'fpath_out': FPATHS['data_brain'],
             'filter_value': 'Mean FA', # dMRI measure
+            'instances': [2],
+            'keep_instance': 'all',
+            'fpath_out': FPATHS['data_brain'],
         },
         'demographic': {
             'categories': [1001, 1002, 1006],
-            'fpath_out': FPATHS['data_demographic'],
             'filter_value': None,
+            'instances': [0, 2],
+            'keep_instance': 'max',
+            'fpath_out': FPATHS['data_demographic'],
         },
     }
 
@@ -41,27 +49,33 @@ if __name__ == '__main__':
         raise ValueError(f'Invalid type "{type}". Accepted types are: {configs.keys()}')
 
     categories = config['categories']
-    fpath_out = config['fpath_out']
     filter_value = config['filter_value']
+    instances = config['instances']
+    keep_instance = config['keep_instance']
+    fpath_out = config['fpath_out']
 
     print('----- Parameters -----')
     print(f'type:\t\t{type}')
     print(f'categories:\t{categories}')
-    print(f'fpath_out:\t{fpath_out}')
+    print(f'value_types:\t{value_types}')
     print(f'filter_value:\t{filter_value}')
+    print(f'instances:\t{instances}')
+    print(f'keep_instance:\t{keep_instance}')
+    print(f'fpath_out:\t{fpath_out}')
+    print(f'chunksize:\t{chunksize}')
     print('----------------------')
 
     field_helper = FieldHelper(dpath_schema)
     udi_helper = UDIHelper(fpath_udis)
 
-    fields = field_helper.get_fields_from_categories(categories)
+    fields = field_helper.get_fields_from_categories(categories, value_types=value_types)
     print(f'Selected {len(fields)} fields')
 
     if filter_value is not None:
         fields = field_helper.filter_by_value(fields, filter_value)
         print(f'{len(fields)} fields remaining after filtering')
 
-    udis = udi_helper.get_udis_from_fields(fields)
+    udis = udi_helper.get_udis_from_fields(fields, instances=instances)
     print(f'Selected {len(udis)} UDIs')
 
     n_rows, n_cols = write_subset(fpath_data, fpath_out, colnames=udis, chunksize=chunksize)
--- src/data_selection.py
+++ src/data_selection.py
@@ -16,14 +16,44 @@ class FieldHelper():
         self.df_recommended = pd.read_csv(self.fpath_recommended, sep='\t')
         self.df_catbrowse = pd.read_csv(self.fpath_catbrowse, sep='\t')
 
-    def __get_fields_from_category(self, category_id):
+    def get_info(self, fields, colnames='all'):
+
+        if colnames == 'all':
+            return self.df_field.set_index('field_id').loc[fields]
+        else:
+            return self.df_field.set_index('field_id').loc[fields, colnames]
+
+    def __get_fields_from_category(self, category_id, value_types='all'):
+
+        # 11: integer
+        # 21: categorical (single)
+        # 22: categorical (multiple)
+        # 31: continuous
+        # 41: text
+        # 51: date
+        # 61: time
+        # 101: compound
+        value_types_all = {11, 21, 22, 31, 41, 51, 61, 101}
+        value_types_numeric = {11, 21, 22, 31}
+
+        if value_types == 'all':
+            value_types = value_types_all
+        elif value_types == 'numeric':
+            value_types = value_types_numeric
+        else:
+            diff = set(value_types) - value_types_all
+
+            if len(diff) != 0:
+                raise ValueError(f'Invalid value types {diff}. Valid calue types are {value_types_all}')
 
         fields = set(self.df_field.loc[self.df_field['main_category'] == category_id, 'field_id'])
         fields.update(self.df_recommended.loc[self.df_recommended['category_id'] == category_id, 'field_id'])
 
+        fields = self.filter_by_list(fields, value_types, 'value_type')
+
         return list(fields)
 
-    def get_fields_from_categories(self, category_ids):
+    def get_fields_from_categories(self, category_ids, value_types='all'):
 
         category_ids = set(category_ids)
         fields = []
@@ -31,21 +61,34 @@ class FieldHelper():
         while len(category_ids) != 0:
             category_id = category_ids.pop()
             category_ids.update(self.df_catbrowse.loc[self.df_catbrowse['parent_id'] == category_id, 'child_id'])
-            fields.extend(self.__get_fields_from_category(category_id))
+            fields.extend(self.__get_fields_from_category(category_id, value_types=value_types))
 
         return list(set(fields)) # removes duplicates
+
+    def filter_by_condition(self, fields, fn_condition, colname):
+
+        df_field_subset = self.df_field.set_index('field_id').loc[fields]
+        df_field_filtered = df_field_subset.loc[df_field_subset[colname].map(fn_condition)]
+
+        return df_field_filtered.index.tolist()
 
     def filter_by_value(self, fields, value, colname='title', keep=True):
 
         if keep:
             fn_condition = (lambda x: value in x)
         else:
-            fn_condition = (lambda x: not (value in x))
+            fn_condition = (lambda x: value not in x)
 
-        df_field_subset = self.df_field.set_index('field_id').loc[fields]
-        df_field_filtered = df_field_subset.loc[df_field_subset[colname].map(fn_condition)]
+        return self.filter_by_condition(fields, fn_condition, colname)
 
-        return df_field_filtered.index.tolist()
+    def filter_by_list(self, fields, value_list, colname, keep=True):
+
+        if keep:
+            fn_condition = lambda x: x in value_list
+        else:
+            fn_condition = lambda x : x not in value_list
+
+        return self.filter_by_condition(fields, fn_condition, colname)
 
 class UDIHelper():
 
@@ -54,15 +97,31 @@ class UDIHelper():
         self.fpath_udis = fpath_udis
         self.df_udis = pd.read_csv(self.fpath_udis)
 
-    def get_udis_from_fields(self, fields, instances='all'):
+    def get_info(self, udis, colnames='all'):
+
+        if colnames == 'all':
+            return self.df_udis.set_index('udi').loc[udis]
+        else:
+            return self.df_udis.set_index('udi').loc[udis, colnames]
+
+    def get_udis_from_fields(self, fields, instances='all', keep_instance='all'):
 
         udis = self.df_udis.loc[self.df_udis['field_id'].isin(fields), 'udi'].tolist()
 
         if instances != 'all':
-            udis = self.filter_by_instance(udis, instances)
+            udis = self.filter_by_instance(udis, instances, keep_instance=keep_instance)
 
         return udis
 
-    def filter_by_instance(self, udis, instances):
+    def filter_by_instance(self, udis, instances, keep_instance='all'):
 
-        return self.df_udis.loc[self.df_udis['udi'].isin(udis) & self.df_udis['instance'].isin(instances), 'udi'].tolist()
+        df_to_keep = self.df_udis.loc[self.df_udis['udi'].isin(udis) & self.df_udis['instance'].isin(instances)]
+
+        if keep_instance == 'all':
+            return df_to_keep.loc[:, 'udi'].tolist()
+        elif keep_instance == 'max':
+            idx = df_to_keep.groupby(['field_id', 'array_index'])['instance'].transform(max) == df_to_keep['instance']
+            return df_to_keep.loc[idx, 'udi'].tolist()
+        else:
+            raise ValueError(f'keep_instance="{keep_instance}" is invalid')
+            
