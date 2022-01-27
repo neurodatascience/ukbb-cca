bee5d2f2a6b5b35e9a6bb337848b9fa971eda2ae
Michelle Wang <tomichellewang@gmail.com>
2022-01-27 11:40:40 -0500

    print script output

--- scripts/select_mri_subjects.py
+++ scripts/select_mri_subjects.py
@@ -9,17 +9,29 @@ def filter_df_by_content(df, colname, value=1):
 if __name__ == '__main__':
 
     field_completed_mri = 12188 # 1: yes, 0: no, -1: unknown
+    instances = [2]
+    chunksize = 10000
+
+    fpath_data = FPATHS['data_tabular_raw']
+    fpath_out = FPATHS['data_tabular_mri_subjects']
 
     fpath_udis = FPATHS['udis_tabular_raw']
-    fpath_data = FPATHS['data_tabular_raw']
+
+    print('----- Parameters -----')
+    print(f'fpath_data:\t{fpath_data}')
+    print(f'fpath_out:\t{fpath_out}')
+    print(f'fpath_udis:\t{fpath_udis}')
+    print(f'field_completed_mri:\t{field_completed_mri}')
+    print(f'instances:\t{instances}')
+    print(f'chunksize:\t{chunksize}')
+    print('----------------------')
 
     udi_helper = UDIHelper(fpath_udis)
+    udi_completed_mri = udi_helper.get_udis_from_fields([field_completed_mri], instances=instances)[0]
 
-    udi_completed_mri = udi_helper.get_udis_from_fields([field_completed_mri], instances=[2])[0]
-
-    fpath_out = FPATHS['data_tabular_mri_subjects']
-    write_subset(
-        fpath_data, fpath_out, colnames=None, chunksize=10,
+    n_rows, n_cols = write_subset(
+        fpath_data, fpath_out, colnames=None, chunksize=chunksize,
         fn_to_apply=(lambda df: filter_df_by_content(df, udi_completed_mri))
     )
 
+    print(f'Wrote {n_rows} rows and {n_cols} columns')
