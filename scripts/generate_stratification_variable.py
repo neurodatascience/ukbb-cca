4d3c2e772f30d132dbbf3f798406097e794cafb7
Michelle Wang <tomichellewang@gmail.com>
2022-02-10 18:25:12 -0500

    do not keep other holdout variables

--- scripts/generate_stratification_variable.py
+++ scripts/generate_stratification_variable.py
@@ -7,47 +7,43 @@ import matplotlib.pyplot as plt
 from src.utils import load_data_df
 from paths import FPATHS, DPATHS
 
+age_udi = '21003-2.0'
 year_group = 5
 
-rename_map = {
-    '34-0.0': 'birth year',
-    '21003-2.0': 'age'
-}
-
 fpath_holdout = FPATHS['data_holdout_clean']
-fpath_out = FPATHS['data_age_clean']
+fpath_out = FPATHS['data_stratification_clean']
 fpath_fig = os.path.join(DPATHS['preprocessing'], 'hist_age.png')
 
 if __name__ == '__main__':
 
     print('----- Parameters -----')
     print(f'year_group:\t{year_group}')
-    print(f'rename_map:\t{rename_map}')
     print(f'fpath_holdout:\t{fpath_holdout}')
     print(f'fpath_out:\t{fpath_out}')
     print('----------------------')
 
     # load holdouts
-    df_age = load_data_df(fpath_holdout, encoded=False).rename(columns=rename_map)
+    df_holdout = load_data_df(fpath_holdout, encoded=False)
 
-    print('Age statistics:')
-    print(f'\tMin: {df_age["age"].min()}')
-    print(f'\tMax: {df_age["age"].max()}')
-    print(f'\tMean: {df_age["age"].mean()}')
-    print(f'\tStd: {df_age["age"].std()}')
-    print(f'\tMedian: {df_age["age"].median()}')
+    print(f'{age_udi} statistics:')
+    print(f'\tMin: {df_holdout[age_udi].min()}')
+    print(f'\tMax: {df_holdout[age_udi].max()}')
+    print(f'\tMean: {df_holdout[age_udi].mean()}')
+    print(f'\tStd: {df_holdout[age_udi].std()}')
+    print(f'\tMedian: {df_holdout[age_udi].median()}')
     print('----------------------')
 
-    # create new column for 5-year period (for stratification)
-    df_age['age_group'] = df_age['age'].map(lambda age: age // year_group)
-    group_ids = set(df_age['age_group'])
+    # get age groups (for stratification) (pandas Series)
+    age_groups = df_holdout[age_udi].map(lambda age: age // year_group)
+    age_groups.name = 'age_group'
+    group_ids = set(age_groups) # unique
 
     print(f'Data has {len(group_ids)} age groups: {group_ids}')
 
     # plot and save age histogram
     fig, ax = plt.subplots(figsize=(4, 4))
     bins = np.arange(5*min(group_ids), 5*(max(group_ids)+2), 5)
-    ax.hist(df_age['age'], bins=bins)
+    ax.hist(df_holdout[age_udi], bins=bins)
     ax.set_title('Age distribution')
     ax.set_xlabel('Age')
     ax.set_ylabel('Count')
@@ -55,5 +51,5 @@ if __name__ == '__main__':
     print(f'Figure saved: {fpath_fig}')
 
     # save
-    df_age.to_csv(fpath_out, header=True, index=True)
+    age_groups.to_csv(fpath_out, header=True, index=True)
     print(f'Dataframe saved: {fpath_out}')
