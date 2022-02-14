d223220ca979dc579effa3dfb13f645bb6525084
Michelle Wang <tomichellewang@gmail.com>
2022-02-14 17:57:14 -0500

    inherit sklearn class and hardcode for 2 domains

--- src/preprocessing_pipeline.py
+++ src/preprocessing_pipeline.py
@@ -1,12 +1,14 @@
 
+from sklearn.utils.metaestimators import _BaseComposition
 from sklearn.pipeline import Pipeline
+
 from sklearn.preprocessing import QuantileTransformer
 from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 
 from src import NanDeconfounder, NanPCA
 
-class PreprocessingPipeline():
+class PreprocessingPipeline(_BaseComposition):
 
     class PipelineXY(Pipeline):
         def transform(self, X, y=None):
@@ -19,29 +21,37 @@ class PreprocessingPipeline():
                     Xt = transform.transform(Xt, y)
             return Xt
 
-    def __init__(self, dataset_names=['data1', 'data2'], conf_name='conf', verbose=False):
+    def __init__(self, 
+        data1_pipeline=None, data2_pipeline=None, conf_pipeline=None, 
+        data1_name='data1', data2_name='data2', conf_name='conf', verbose=False
+    ):
 
-        self.dataset_names = dataset_names
+        def build_data_pipeline():
+            data_steps = [
+                (f'inv_norm', QuantileTransformer(output_distribution='normal')),
+                (f'scaler', StandardScaler()),
+                (f'deconfounder', NanDeconfounder()),
+                (f'pca', NanPCA()),
+            ]
+            return self.PipelineXY(data_steps, verbose=verbose)
+
+        self.data1_name = data1_name
+        self.data2_name = data2_name
         self.conf_name = conf_name
         self.verbose = verbose
 
+        # TODO move this to main CV script
         conf_steps = [
-            ('conf_imputer', SimpleImputer(strategy='median')),
-            ('conf_inv_norm', QuantileTransformer(output_distribution='normal')),
-            ('conf_scaler', StandardScaler()),
+            ('imputer', SimpleImputer(strategy='median')),
+            ('inv_norm', QuantileTransformer(output_distribution='normal')),
+            ('scaler', StandardScaler()),
         ]
-
         self.conf_pipeline = Pipeline(conf_steps, verbose=verbose)
+        self.data1_pipeline = build_data_pipeline()
+        self.data2_pipeline = build_data_pipeline()
 
-        self.data_pipelines = {}
-        for dataset_name in dataset_names:
-            data_steps = [
-                (f'{dataset_name}_inv_norm', QuantileTransformer(output_distribution='normal')),
-                (f'{dataset_name}_scaler', StandardScaler()),
-                (f'{dataset_name}_deconfounder', NanDeconfounder()),
-                (f'{dataset_name}_pca', NanPCA()),
-            ]
-            self.data_pipelines[dataset_name] = self.PipelineXY(data_steps, verbose=verbose)
+        self.dataset_names = [data1_name, data2_name]
+        self.data_pipelines = {data1_name: self.data1_pipeline, data2_name: self.data2_pipeline}
 
     def fit(self, X, y=None):
         self._fit(X)
@@ -88,6 +98,8 @@ class PreprocessingPipeline():
     def _check_X(self, X):
         # top level unique keys (except for conf_name)
         dataset_names = set(X.drop(columns=self.conf_name, errors='ignore').columns.get_level_values(0))
+        if len(dataset_names) != 2:
+            raise ValueError(f'X must contain exactly 2 datasets (excluding confounders)')
         if dataset_names != set(self.dataset_names):
             raise ValueError(f'Mismatch between dataset names. Expected {self.dataset_names} (conf: {self.conf_name}), got {dataset_names})')
 
