d0a15de7b9b6deecb97d070e27caa1fd24bbcb19
Michelle Wang <tomichellewang@gmail.com>
2022-02-14 19:31:30 -0500

    move pipeline definitions out of class

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 
 from .nan_deconfounder import NanDeconfounder
 from .nan_pca import NanPCA
-from .preprocessing_pipeline import PreprocessingPipeline
+from .preprocessing_pipeline import PreprocessingPipeline, PipelineXY
--- src/preprocessing_pipeline.py
+++ src/preprocessing_pipeline.py
@@ -2,55 +2,38 @@
 from sklearn.utils.metaestimators import _BaseComposition
 from sklearn.pipeline import Pipeline
 
-from sklearn.preprocessing import QuantileTransformer
-from sklearn.preprocessing import StandardScaler
-from sklearn.impute import SimpleImputer
-
-from src import NanDeconfounder, NanPCA
+class PipelineXY(Pipeline):
+    def transform(self, X, y=None):
+        '''Same as transform() in Pipeline class but accepts y variable (needed in custom transformer classes).'''
+        Xt = X
+        for _, _, transform in self._iter():
+            try:
+                Xt = transform.transform(Xt)
+            except TypeError: # missing positional argument
+                Xt = transform.transform(Xt, y)
+        return Xt
 
 class PreprocessingPipeline(_BaseComposition):
 
-    class PipelineXY(Pipeline):
-        def transform(self, X, y=None):
-            '''Same as transform() in Pipeline class but accepts y variable (needed in custom transformer classes).'''
-            Xt = X
-            for _, _, transform in self._iter():
-                try:
-                    Xt = transform.transform(Xt)
-                except TypeError: # missing positional argument
-                    Xt = transform.transform(Xt, y)
-            return Xt
-
     def __init__(self, 
-        data1_pipeline=None, data2_pipeline=None, conf_pipeline=None, 
+        data1_pipeline, data2_pipeline, conf_pipeline=None, 
         data1_name='data1', data2_name='data2', conf_name='conf', verbose=False
     ):
-
-        def build_data_pipeline():
-            data_steps = [
-                (f'inv_norm', QuantileTransformer(output_distribution='normal')),
-                (f'scaler', StandardScaler()),
-                (f'deconfounder', NanDeconfounder()),
-                (f'pca', NanPCA()),
-            ]
-            return self.PipelineXY(data_steps, verbose=verbose)
 
         self.data1_name = data1_name
         self.data2_name = data2_name
         self.conf_name = conf_name
         self.verbose = verbose
 
-        # TODO move this to main CV script
-        conf_steps = [
-            ('imputer', SimpleImputer(strategy='median')),
-            ('inv_norm', QuantileTransformer(output_distribution='normal')),
-            ('scaler', StandardScaler()),
-        ]
-        self.conf_pipeline = Pipeline(conf_steps, verbose=verbose)
-        self.data1_pipeline = build_data_pipeline()
-        self.data2_pipeline = build_data_pipeline()
+        # need to be defined as input arguments in __init__
+        # for pipeline get_params()/set_params() to work
+        self.conf_pipeline = conf_pipeline
+        self.data1_pipeline = data1_pipeline
+        self.data2_pipeline = data2_pipeline
 
+        # for input validation
         self.dataset_names = [data1_name, data2_name]
+        # for looping
         self.data_pipelines = {data1_name: self.data1_pipeline, data2_name: self.data2_pipeline}
 
     def fit(self, X, y=None):
@@ -69,7 +52,6 @@ class PreprocessingPipeline(_BaseComposition):
 
             if self.verbose:
              print(f'[Preprocessing pipeline] Processing {dataset_name}')
-
             self.data_pipelines[dataset_name].fit(X[dataset_name], conf_preprocessed)
 
         return conf_preprocessed
@@ -108,6 +90,9 @@ class PreprocessingPipeline(_BaseComposition):
         try:
             conf = X[self.conf_name]
         except KeyError:
+            return None
+
+        if self.conf_pipeline is None:
             return None
 
         if self.verbose:
