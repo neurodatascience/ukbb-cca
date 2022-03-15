55703072aa352307582b840ea501e5d8ab3047e0
Michelle Wang <tomichellewang@gmail.com>
2022-03-15 15:24:49 -0400

    use preprocessed (PCA) data

--- scripts/bootstrap_cca.py
+++ scripts/bootstrap_cca.py
@@ -12,13 +12,13 @@ from paths import DPATHS, FPATHS
 # parameters
 n_permutations = 1000
 seed = None # for random number generator
-verbose=True
 
 # paths to data files
-fpath_data_train = FPATHS['data_Xy_train']
+dpath_data_preprocessed = DPATHS['cca_preprocessed'] # directory containing preprocessed train data
 fpath_data_test = FPATHS['data_Xy_test']
+dpath_preprocessor = DPATHS['cca_preprocessor']
 fpath_holdout = FPATHS['data_holdout_clean'] # for bootstrapped regression with holdout variable
-holdout_udi = '21003-2.0'
+udi_holdout = '21003-2.0'
 
 # output path
 dpath_out = DPATHS['cca']
@@ -27,54 +27,59 @@ fname_out_prefix = 'bootstrap_results'
 if __name__ == '__main__':
 
     # process user inputs
-    print(len(sys.argv), sys.argv)
     if len(sys.argv) < 3:
-        raise ValueError(f'Usage: {sys.argv[0]} <n_components1> <n_components2> [etc.]')
+        print(f'Usage: {sys.argv[0]} <n_components1> <n_components2> [etc.]')
+        sys.exit(1)
     n_components_all = [int(n) for n in sys.argv[1:]] # number of PCA components
 
-    suffix = '_'.join([str(n) for n in n_components_all])
-    fpath_out = os.path.join(dpath_out, f'{fname_out_prefix}_{suffix}.pkl')
+    str_components = '_'.join([str(n) for n in n_components_all])
+    fpath_data_train_preprocessed = os.path.join(dpath_data_preprocessed, f'X_train_{str_components}.pkl')
+    fpath_preprocessor = os.path.join(dpath_preprocessor, f'preprocessor_{str_components}.pkl')
+    fpath_out = os.path.join(dpath_out, f'{fname_out_prefix}_{str_components}.pkl')
 
     print('----- Parameters -----')
     print(f'n_permutations:\t{n_permutations}')
     print(f'seed:\t{seed}')
     print(f'n_components_all:\t{n_components_all}')
-    print(f'verbose:\t{verbose}')
-    print(f'fpath_data_train:\t{fpath_data_train}')
+    print(f'udi_holdout:\t{udi_holdout}')
+    print(f'fpath_preprocessor:\t{fpath_preprocessor}')
+    print(f'fpath_data_train_preprocessed:\t{fpath_data_train_preprocessed}')
     print(f'fpath_data_test:\t{fpath_data_test}')
+    print(f'fpath_holdout:\t{fpath_holdout}')
     print('----------------------')
 
     # create random number generator
     rng = np.random.default_rng(seed)
 
     # load train data
-    with open(fpath_data_train, 'rb') as file_train:
-        train_data = pickle.load(file_train)
-        X_train = train_data['X']
-        dataset_names = train_data['dataset_names']
-        n_datasets = len(dataset_names)
-        conf_name = train_data['conf_name']
+    with open(fpath_data_train_preprocessed, 'rb') as file_in:
+        preprocessed_data = pickle.load(file_in)
+    X_train_preprocessed = preprocessed_data['X'] # already preprocessed
+    dataset_names = preprocessed_data['dataset_names']
+    n_datasets = preprocessed_data['n_datasets']
+    conf_name = preprocessed_data['conf_name']
+    subjects_train = preprocessed_data['subjects']
+    n_subjects_train = len(subjects_train)
 
-    # load test data
+    # load preprocessor
+    with open(fpath_preprocessor, 'rb') as file_in:
+        fitted_preprocessor = pickle.load(file_in)['preprocessor']
+
+    print('------------------------------------------------------------------')
+    print(fitted_preprocessor)
+    print('------------------------------------------------------------------')
+
+    # load and preprocess test data
     with open(fpath_data_test, 'rb') as file_test:
         test_data = pickle.load(file_test)
-        X_test = test_data['X']
-
-    subjects_train = X_train.index
+    X_test = test_data['X']
     subjects_test = X_test.index
-    n_subjects_train = len(subjects_train)
+    X_test_preprocessed = fitted_preprocessor.transform(X_test)
 
     # load holdout data
     df_holdout = load_data_df(fpath_holdout)
-    holdout_train = df_holdout.loc[subjects_train, holdout_udi]
-    holdout_test = df_holdout.loc[subjects_test, holdout_udi]
-
-    # process PCA n_components
-    if len(n_components_all) != n_datasets:
-        raise ValueError(f'Mismatch between n_components_all (size {len(n_components_all)}) and dataset_names (size {len(dataset_names)})')
-    for i_dataset, dataset_name in enumerate(dataset_names):
-        if n_components_all[i_dataset] is None:
-            n_components_all[i_dataset] = X_train[dataset_name].shape[1]
+    holdout_train = df_holdout.loc[subjects_train, udi_holdout]
+    holdout_test = df_holdout.loc[subjects_test, udi_holdout]
 
     # figure out the number of latent dimensions in CCA
     n_latent_dims = min(n_components_all)
@@ -86,18 +91,11 @@ if __name__ == '__main__':
         dataset_names=dataset_names,
         n_pca_components_all=n_components_all,
         cca__latent_dims=n_latent_dims,
-        verbose=verbose,
     )
-    print('------------------------------------------------------------------')
-    print(cca_pipeline)
-    print('------------------------------------------------------------------')
-
-    preprocessor = cca_pipeline['preprocessor']
     cca = cca_pipeline['cca']
-
-    # preprocess data
-    X_train_preprocessed = preprocessor.fit_transform(X_train)
-    X_test_preprocessed = preprocessor.fit_transform(X_test)
+    print('------------------------------------------------------------------')
+    print(cca)
+    print('------------------------------------------------------------------')
 
     # 'fit' cca (weights will be overwritten later)
     cca.fit_transform(X_train_preprocessed)
@@ -159,7 +157,6 @@ if __name__ == '__main__':
 
     # to be pickled
     results_all = {
-        'model': cca_pipeline, # fitted model
         'correlations_bootstrap_train': np.array(correlations_bootstrap_train).T,
         'correlations_bootstrap_test': np.array(correlations_bootstrap_test).T,
         'R2_holdout_bootstrap_train': np.array(R2_holdout_bootstrap_train).T,
