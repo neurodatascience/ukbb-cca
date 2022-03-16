30a74106d2aaef09dadc1243fd16e4f22a3aff16
Michelle Wang <tomichellewang@gmail.com>
2022-03-16 11:41:48 -0400

    bug fix and plot sample distributions

--- scripts/combine_cv_results.py
+++ scripts/combine_cv_results.py
@@ -1,9 +1,13 @@
 
 import sys, os, glob, pickle
 import numpy as np
+import matplotlib.pyplot as plt
 from paths import DPATHS
 
 save_extracted = True # if True, saved only summary (e.g., mean/median) measures instead of everything
+plot_sample_distributions = True
+n_to_plot = (5, 5)
+ax_size = 3
 
 dpath_cv = dpath_cv = os.path.join(DPATHS['scratch'], os.path.basename(DPATHS['cca'])) # folder containing CV results (different parameters/runs)
 cv_filename_pattern = '*rep*.pkl'
@@ -55,19 +59,19 @@ if __name__ == '__main__':
             udis = results['udis_datasets']
             n_folds = results['n_folds']
 
-            projections_val = results['projections_val']
-            loadings_val = results['loadings_val']
-            weights_train = results['weights_train']
-
-            correlations_val = results['correlations_val']
-            R2_PC_reg_val = results['R2_PC_reg_val']
-
             # initialization
             projections_combined = [[] for _ in range(n_datasets)]
             loadings_combined = [[] for _ in range(n_datasets)]
             weights_combined = [[] for _ in range(n_datasets)]
             correlations_val_combined = []
             R2_PC_reg_val_combined = []
+
+        correlations_val = results['correlations_val']
+        R2_PC_reg_val = results['R2_PC_reg_val']
+
+        projections_val = results['projections_val']
+        loadings_val = results['loadings_val']
+        weights_train = results['weights_train']
 
         for i_dataset in range(n_datasets):
             projections_combined[i_dataset].append(projections_val[i_dataset])
@@ -129,4 +133,39 @@ if __name__ == '__main__':
     fpath_out = os.path.join(dpath_out, f'{dname_reps}_results_{out_suffix}.pkl')
     with open(fpath_out, 'wb') as file_out:
         pickle.dump(results_to_save, file_out)
+    print(f'Saved to {fpath_out}')
 
+    # optional plotting
+    if plot_sample_distributions:
+
+        dpath_figs = os.path.join(dpath_out, 'figs')
+        n_rows, n_cols = n_to_plot
+        
+        to_plot = {}
+        for label, data in {'projections': projections_combined, 'loadings': loadings_combined, 'weights': weights_combined}.items():
+            to_plot.update({f'{label}_{dataset_name}': data[i_dataset][:, :n_rows, :n_cols] for i_dataset, dataset_name in enumerate(dataset_names)})
+        
+        with open(os.path.join(dpath_figs, f'plot_data_{dname_reps}.pkl'), 'wb') as file_out:
+            pickle.dump(to_plot, file_out)
+
+        for label, data in to_plot.items():
+            fig, axes = plt.subplots(
+                nrows=n_rows, ncols=n_cols, 
+                figsize=(n_cols*ax_size, n_rows*ax_size),
+                sharey='all',
+            )
+            if n_rows == 1:
+                axes = [axes]
+            if n_cols == 1:
+                axes = [axes]
+
+            for i_row in range(n_rows):
+                for i_col in range(n_cols):
+                    ax = axes[i_row][i_col]
+                    ax.hist(data[:, i_row, i_col], bins=25)
+            
+            fig.tight_layout()
+
+            fpath_fig = os.path.join(dpath_figs, f'{label}_{dname_reps}.png')
+            fig.savefig(fpath_fig, dpi=300, bbox_inches='tight')
+            print(f'Saved figure to {fpath_fig}')
