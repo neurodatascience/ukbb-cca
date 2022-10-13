bd2c4c112b3bb7a139fed183cf6e8ba9ea8d1510
Michelle Wang <tomichellewang@gmail.com>
2022-10-13 15:03:04 -0400

    add "domain" and "squared" parameters

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -4,14 +4,14 @@ from src.database_helpers import DatabaseHelper
 from src.data_processing import clean_datasets
 from src.utils import print_params
 
-# process all 3 datasets at the same time
-# so that they all keep/remove the same subjects
 DOMAIN_DEMOGRAPHIC = 'demographic'
 DOMAINS = ['behavioural', 'brain', DOMAIN_DEMOGRAPHIC]
 
 @click.command()
 @click.argument('dpath-data', required=True, default='.', envvar='DPATH_PROCESSED')
 @click.argument('dpath-figs', required=True, default='.', envvar='DPATH_PREPROCESSING')
+@click.option('--domain', 'domains', multiple=True)
+@click.option('--squared', 'squared', multiple=True)
 @click.option('--holdout', 'holdouts', multiple=True, default=[21003, 34]) # age, year of birth
 @click.option('--square-conf', default=True)
 @click.option('--threshold-na', default=0.5)
@@ -19,14 +19,16 @@ DOMAINS = ['behavioural', 'brain', DOMAIN_DEMOGRAPHIC]
 @click.option('--threshold-outliers', default=100)
 @click.option('--dpath-schema', required=True, envvar='DPATH_SCHEMA')
 @click.option('--fpath-udis', required=True, envvar='FPATH_UDIS')
-def clean_data(dpath_data, dpath_figs, holdouts, square_conf, 
+def clean_data(dpath_data, dpath_figs, domains, squared, holdouts, square_conf, 
     threshold_na, threshold_high_freq, threshold_outliers,
     dpath_schema, fpath_udis
 ):
 
     db_helper = DatabaseHelper(dpath_schema, fpath_udis)
-    domains = DOMAINS
-    domains_square = [DOMAIN_DEMOGRAPHIC]
+    if len(domains) == 0:
+        domains = DOMAINS
+    if len(squared) == 0:
+        squared = [DOMAIN_DEMOGRAPHIC]
 
     print_params(locals())
 
@@ -37,7 +39,7 @@ def clean_data(dpath_data, dpath_figs, holdouts, square_conf,
         domains=domains,
         holdout_fields=holdouts,
         square_conf=square_conf,
-        domains_square=domains_square,
+        domains_square=squared,
         threshold_na=threshold_na,
         threshold_high_freq=threshold_high_freq,
         threshold_outliers=threshold_outliers,
