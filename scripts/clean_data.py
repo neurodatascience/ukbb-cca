b2fcd718d13ea7b2e860c066ed8aa9f5f58a81c6
Michelle Wang <tomichellewang@gmail.com>
2022-10-13 13:22:29 -0400

    print parameters

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -2,6 +2,7 @@
 import click
 from src.database_helpers import DatabaseHelper
 from src.data_processing import clean_datasets
+from src.utils import print_params
 
 # process all 3 datasets at the same time
 # so that they all keep/remove the same subjects
@@ -22,33 +23,25 @@ def clean_data(dpath_data, dpath_figs, holdouts, square_conf,
     threshold_na, threshold_high_freq, threshold_outliers,
     dpath_schema, fpath_udis
 ):
+
     db_helper = DatabaseHelper(dpath_schema, fpath_udis)
+    domains = DOMAINS
+    domains_square = [DOMAIN_DEMOGRAPHIC]
+
+    print_params(locals())
+
     clean_datasets(
         db_helper=db_helper,
         dpath_data=dpath_data,
         dpath_figs=dpath_figs,
-        domains=DOMAINS,
+        domains=domains,
         holdout_fields=holdouts,
         square_conf=square_conf,
-        domains_square=[DOMAIN_DEMOGRAPHIC],
+        domains_square=domains_square,
         threshold_na=threshold_na,
         threshold_high_freq=threshold_high_freq,
         threshold_outliers=threshold_outliers,
     )
 
 if __name__ == '__main__':
-
     clean_data()
-
-    # print('----- Parameters -----')
-    # print(f'domains:\t{DOMAINS}')
-    # print(f'holdout_fields:\t{holdout_fields}')
-    # print(f'threshold_na:\t{threshold_na}')
-    # print(f'threshold_high_freq:\t{threshold_high_freq}')
-    # print(f'threshold_outliers:\t{threshold_outliers}')
-    # print(f'fpath_holdout:\t{fpath_holdout}')
-    # print(f'fpath_dropped_subjects:\t{fpath_dropped_subjects}')
-    # print(f'fpath_dropped_udis:\t{fpath_dropped_udis}')
-    # print(f'fig_prefix:\t{FIG_PREFIX}')
-    # print(f'dpath_figs:\t{dpath_figs}')
-    # print('----------------------')
