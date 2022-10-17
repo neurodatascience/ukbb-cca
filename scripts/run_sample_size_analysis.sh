341ae48c8200a7cf28d101cbcf725ad28d8aa38e
Michelle Wang <tomichellewang@gmail.com>
2022-10-17 14:52:46 -0400

    add command-line arguments

--- scripts/run_sample_size_analysis.sh
+++ scripts/run_sample_size_analysis.sh
@@ -1,22 +1,74 @@
 #!/bin/bash
 
-N_PCS="100 100"
-
-I_SAMPLE_SIZE_MIN=1
-I_SAMPLE_SIZE_MAX=20
-
-I_BOOTSTRAP_REPETITION_MIN=1
-I_BOOTSTRAP_REPETITION_MAX=50
-
-N_SAMPLE_SIZES=30
-N_BOOTSTRAP_REPETITIONS=100
-
 FNAME_DISPATH_SCRIPT="dispatch_job.sh" # assumes all scripts are in same directory
 FNAME_CCA_SCRIPT="cca_sample_size.py"
 
-DPATH_SCRIPTS=`dirname $0`
+USAGE="Usage: $0 -s/--sample-sizes MIN MAX TOTAL -b/--bootstrap-repetitions MIN MAX TOTAL -p/--pcs N PC1 PC2 [...]"
+
+# get path to slurm script directory
+if [ -z "${SLURM_JOB_ID}" ]
+then
+    FPATH_CURRENT="$0"
+else
+    # check the original location through scontrol and $SLURM_JOB_ID
+    FPATH_CURRENT=`scontrol show job "${SLURM_JOB_ID}" | awk -F= '/Command=/{print $2}' | awk '{print $1}'`
+fi
+FPATH_CURRENT=`realpath ${FPATH_CURRENT}`
+DPATH_SCRIPTS=`dirname ${FPATH_CURRENT}`
 FPATH_DISPATCH_SCRIPT="${DPATH_SCRIPTS}/${FNAME_DISPATH_SCRIPT}"
 FPATH_CCA_SCRIPT="${DPATH_SCRIPTS}/${FNAME_CCA_SCRIPT}"
+
+if [ "$#" -lt 12 ]
+then
+    echo $USAGE
+    exit 1
+fi
+
+# parse args
+while [[ "$#" -gt 0 ]]
+do
+    case $1 in
+        -s|--sample_sizes)
+            I_SAMPLE_SIZE_MIN="$2"
+            I_SAMPLE_SIZE_MAX="$3"
+            N_SAMPLE_SIZES="$4"
+            shift # past argument
+            shift # past value
+            shift # past value
+            shift # past value
+            ;;
+        -b|--bootstrap-repetitions)
+            I_BOOTSTRAP_REPETITION_MIN="$2"
+            I_BOOTSTRAP_REPETITION_MAX="$3"
+            N_BOOTSTRAP_REPETITIONS="$4"
+            shift # past argument
+            shift # past value
+            shift # past value
+            shift # past value
+            ;;
+        -p|--pcs)
+            n="$2" # number of args to take
+            shift # past argument
+            shift # past n
+            for (( i=0; i < ${n}; i += 1 ))
+            do
+                N_PC="$1"
+                if [ -z "${N_PCS}" ]
+                then
+                    N_PCS="${N_PC}"
+                else
+                    N_PCS="${N_PCS} ${N_PC}"
+                fi
+                shift
+            done
+            ;;
+        *)
+            echo "Invalid input argument: $1"
+            echo $USAGE
+            exit 1
+            ;;
+    esac
+done
 
 if [ -z $FPATH_DISPATCH_SCRIPT ]
 then
@@ -52,6 +104,7 @@ do
         SUBDIRS_LOG="PCs_${N_PCS// /_}/i_sample_size_${I_SAMPLE_SIZE}"
         
         COMMAND="${FPATH_DISPATCH_SCRIPT} ${FPATH_CCA_SCRIPT} -m ${DISPATCH_MEMORY} -t ${DISPATCH_TIME} -d ${SUBDIRS_LOG} ${N_SAMPLE_SIZES} ${N_BOOTSTRAP_REPETITIONS} ${I_SAMPLE_SIZE} ${I_BOOTSTRAP_REPETITION} ${N_PCS}"
+        echo ${COMMAND}
         eval ${COMMAND}
     done
 done
